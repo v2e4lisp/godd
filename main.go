d4e652f314da38c8cf44a1d0b58959f5c2ea2796
wenjun.yan <mylastnameisyan@gmail.com>
2015-04-25 00:40:11 +0900

    update

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
         "os"
         "os/exec"
         "os/signal"
+        "path/filepath"
         "regexp"
         "strings"
         "sync"
@@ -16,37 +17,38 @@ import (
 var (
         procpat  = regexp.MustCompile("^[a-zA-z0-9_]+$")
         procfile = "Procfile"
-        envfile  = ".env"
-        procs    = make(map[string]string)
-        start    string
 )
 
-func loadProcs() error {
-        file, err := os.Open("Procfile")
+// type Command struct {
+//         cmd  *exec.Cmd
+//         name string
+// }
+
+func loadProcs(procfile string) (map[string]string, error) {
+        file, err := os.Open(procfile)
         if err != nil {
-                return err
+                return nil, err
         }
         defer file.Close()
 
+        procs := make(map[string]string)
         s := bufio.NewScanner(file)
+        ln := 0
         for s.Scan() {
+                ln++
                 l := strings.TrimSpace(s.Text())
                 if l == "" || l[0] == '#' {
                         continue
                 }
                 p := strings.SplitN(l, ":", 2)
                 if len(p) != 2 || !procpat.Match([]byte(p[0])) {
-                        return errors.New("malformat process: " + l)
+                        msg := fmt.Sprintf("parsing error at %s#%d:\n\t%s", procfile, ln, l)
+                        return nil, errors.New(msg)
                 }
                 procs[p[0]] = strings.TrimSpace(p[1])
         }
 
-        return nil
-}
-
-func loadEnv() error {
-        // pass
-        return nil
+        return procs, nil
 }
 
 func run(cmds []*exec.Cmd) {
@@ -100,41 +102,73 @@ func run(cmds []*exec.Cmd) {
         }
 }
 
+func abort(msg ...interface{}) {
+        fmt.Println(msg...)
+        os.Exit(1)
+}
+
 func main() {
+        flag.StringVar(&procfile, "procfile", "Procfile", "specify Procfile")
         flag.Parse()
+
+        var err error
+        procfile, err = filepath.Abs(procfile)
+        if err != nil {
+                abort("Procfile", procfile, err.Error())
+        }
+
         if flag.NArg() < 1 {
-                fmt.Println("godd subcomand ...")
+                flag.Usage()
                 os.Exit(1)
         }
-        args := flag.Args()
-        switch args[0] {
+
+        switch flag.Arg(0) {
         case "start":
-                if len(args) > 2 {
-                        fmt.Println("godd start [process]")
-                        os.Exit(1)
-                }
-                if len(args) == 2 {
-                        start = args[1]
-                }
+                doStart()
+        case "check":
+                doCheck()
+        default:
+                abort("command not found:", flag.Arg(0))
+        }
+}
+
+func doStart() {
+        proc := ""
+        if flag.NArg() > 2 {
+                abort("godd [-p /path/to/procfile] start [proc]")
+        }
+        if flag.NArg() == 2 {
+                proc = flag.Arg(1)
         }
 
-        if err := loadProcs(); err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
-
-        if err := loadEnv(); err != nil {
-                fmt.Println(err)
-                os.Exit(1)
+        procs, err := loadProcs(procfile)
+        if err != nil {
+                abort("Procfile", err)
         }
 
         cmds := []*exec.Cmd(nil)
         for name, cmd := range procs {
                 c := strings.Split(cmd, " ")
-                if start == "" || start == name {
+                if proc == "" || proc == name {
                         cmds = append(cmds, exec.Command(c[0], c[1:]...))
                 }
         }
 
+        if len(procs) > 0 && len(cmds) == 0 && proc != "" {
+                abort("proc not found:", proc)
+        }
+
         run(cmds)
+}
+
+func doCheck() {
+        if flag.NArg() > 1 {
+                abort("godd [-p /path/to/procfile] check")
+        }
+
+        _, err := loadProcs(procfile)
+        if err != nil {
+                abort("Procfile", err)
+        }
+        fmt.Println("OK")
 }
