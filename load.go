95da8ef3464e0e3e9227d53aedde23170f6a5c37
wenjun.yan <mylastnameisyan@gmail.com>
2015-05-10 00:10:41 +0900

    split code

---
+++ load.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+        "errors"
+        "fmt"
+        "io/ioutil"
+        "strings"
+)
+
+func LoadEnv(envfile string) ([]string, error) {
+        env := []string(nil)
+        text, err := ioutil.ReadFile(envfile)
+        if err != nil {
+                return nil, err
+        }
+        lines := strings.Split(string(text), "\n")
+
+        for ln, l := range lines {
+                if l == "" || l[0] == '#' {
+                        continue
+                }
+
+                e := strings.SplitN(l, "=", 2)
+                if len(e) != 2 || !envNamePat.Match([]byte(e[0])) {
+                        msg := fmt.Sprintf("parsing error at %s#%d:\n\t%s",
+                                envfile, ln+1, l)
+                        return nil, errors.New(msg)
+                }
+
+                key := e[0]
+                val := strings.TrimSpace(e[1])
+                last := len(val) - 1
+                if val[0] == '\'' && val[last] == '\'' {
+                        val = val[1:last]
+                } else if val[0] == '"' && val[last] == '"' {
+                        val = val[1:last]
+                        val = strings.Replace(val, "\\\"", "\"", -1)
+                        val = strings.Replace(val, "\\n", "\n", -1)
+                }
+
+                env = append(env, key+"="+val)
+        }
+
+        return env, nil
+}
+
+func LoadProcs(procfile string) (map[string]string, error) {
+        procs := make(map[string]string)
+        text, err := ioutil.ReadFile(procfile)
+        if err != nil {
+                return nil, err
+        }
+        lines := strings.Split(string(text), "\n")
+
+        for ln, l := range lines {
+                if l == "" || l[0] == '#' {
+                        continue
+                }
+                p := strings.SplitN(l, ":", 2)
+                if len(p) != 2 || !procNamePat.Match([]byte(p[0])) {
+                        msg := fmt.Sprintf("parsing error at %s#%d:\n\t%s",
+                                procfile, ln+1, l)
+                        return nil, errors.New(msg)
+                }
+                procs[p[0]] = strings.TrimSpace(p[1])
+        }
+
+        return procs, nil
+}
---
+++ run.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"os/signal"
+	"regexp"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var (
+	procNamePat = regexp.MustCompile("^[a-zA-z0-9_]+$")
+	envNamePat  = regexp.MustCompile("^[a-zA-z0-9_]+$")
+	waitKill    = 3 * time.Second
+)
+
+type Command struct {
+	name string
+	c    *exec.Cmd
+}
+
+func Run(cmds []*Command) {
+	if len(cmds) == 0 {
+		return
+	}
+	logInit(cmds)
+
+	// broadcast to kill all commands' processes
+	kill := make(chan bool)
+	// any command finished
+	done := make(chan bool, len(cmds))
+	// handle Ctrl-C and other signals
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
+	var wg sync.WaitGroup
+	for _, cmd := range cmds {
+		if err := logging(cmd); err != nil {
+			log(cmd.name, "unable to redirect stderr and stdout", err)
+		}
+		// If you get this error, chances are the `sh' is not found
+		if err := cmd.c.Start(); err != nil {
+			log(cmd.name, err)
+			done <- true
+			break
+		}
+		wg.Add(1)
+		log(cmd.name, "[STARTED] pid:", cmd.c.Process.Pid)
+
+		exit := make(chan error)
+		go func(cmd *Command, exit chan error) {
+			exit <- cmd.c.Wait()
+		}(cmd, exit)
+
+		// To prevent killing a terminated command,
+		// send a message to the `done' channel and exit the goroutine
+		go func(cmd *Command, exit chan error) {
+			defer wg.Done()
+			defer func() { done <- true }()
+			defer func() { log(cmd.name, "[EXITED]", cmd.c.ProcessState) }()
+
+			select {
+			case <-exit:
+			case <-kill:
+				syslog("sending SIGTERM to", cmd.name)
+				cmd.c.Process.Signal(syscall.SIGTERM)
+				// if SIGTERM cannot kill the process,
+				// send it a SIGKILL
+				select {
+				case <-exit:
+				case <-time.After(waitKill):
+					syslog("sending SIGKILL to", cmd.name)
+					cmd.c.Process.Kill()
+				}
+			}
+		}(cmd, exit)
+	}
+
+	select {
+	case <-done:
+		close(kill)
+	case <-sigs:
+		close(kill)
+	}
+
+	wg.Wait()
+}
