54f9c9517052fe9af9dd1da3ba6b0e0bc75c0ae1
wenjun.yan <mylastnameisyan@gmail.com>
2015-06-29 18:15:07 +0900

    use log.Println

--- run.go
+++ run.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"log"
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 	"sync"
 	"syscall"
 	"time"
@@ -18,11 +21,35 @@ type Command struct {
 	c    *exec.Cmd
 }
 
+func logging(cmd *Command) error {
+	stdout, err := cmd.c.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := cmd.c.StderrPipe()
+	if err != nil {
+		return err
+	}
+	bufout, buferr := bufio.NewReader(stdout), bufio.NewReader(stderr)
+	pipe := func(b *bufio.Reader) {
+		for {
+			line, err := b.ReadBytes('\n')
+			if err != nil {
+				break
+			}
+			log.Println(cmd.name, strings.TrimSpace(string(line)))
+		}
+	}
+
+	go pipe(bufout)
+	go pipe(buferr)
+	return nil
+}
+
 func Run(cmds []*Command) {
 	if len(cmds) == 0 {
 		return
 	}
-	logInit(cmds)
 
 	// broadcast to kill all commands' processes
 	kill := make(chan bool)
@@ -35,40 +62,37 @@ func Run(cmds []*Command) {
 	var wg sync.WaitGroup
 	for _, cmd := range cmds {
 		if err := logging(cmd); err != nil {
-			log(cmd.name, "unable to redirect stderr and stdout", err)
+			log.Println(cmd.name, "unable to redirect stderr and stdout", err)
 		}
 		// If you get this error, chances are the `sh' is not found
 		if err := cmd.c.Start(); err != nil {
-			log(cmd.name, err)
+			log.Println(cmd.name, err)
 			done <- true
 			break
 		}
-		wg.Add(1)
-		log(cmd.name, "[STARTED] pid:", cmd.c.Process.Pid)
+		log.Println(cmd.name, "[STARTED] pid:", cmd.c.Process.Pid)
 
 		exit := make(chan error)
-		go func(cmd *Command, exit chan error) {
-			exit <- cmd.c.Wait()
-		}(cmd, exit)
-
+		go func(cmd *Command, exit chan error) { exit <- cmd.c.Wait() }(cmd, exit)
 		// To prevent killing a terminated command,
 		// send a message to the `done' channel and exit the goroutine
 		go func(cmd *Command, exit chan error) {
+			wg.Add(1)
 			defer wg.Done()
 			defer func() { done <- true }()
-			defer func() { log(cmd.name, "[EXITED]", cmd.c.ProcessState) }()
+			defer func() { log.Println(cmd.name, "[EXITED]", cmd.c.ProcessState) }()
 
 			select {
 			case <-exit:
 			case <-kill:
-				syslog("sending SIGTERM to", cmd.name)
+				log.Println("sys", "sending SIGTERM to", cmd.name)
 				cmd.c.Process.Signal(syscall.SIGTERM)
 				// if SIGTERM cannot kill the process,
 				// send it a SIGKILL
 				select {
 				case <-exit:
 				case <-time.After(waitKill):
-					syslog("sending SIGKILL to", cmd.name)
+					log.Println("sys", "sending SIGKILL to", cmd.name)
 					cmd.c.Process.Kill()
 				}
 			}
